0b46ee81beda480580667a9fc3d17ebc1bbc82c4
Hao Peng <haop@allennlp-cirrascale-01.reviz.ai2.in>
2022-10-01 00:31:20 +0000

    dependencies and some tmp files

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from distutils.core import setup, Extension
+import numpy as np
+
+os.environ["CC"] = "g++"
+os.environ["CXX"] = "g++"
+
+if 'CUDA_PATH' in os.environ:
+   CUDA_PATH = os.environ['CUDA_PATH']
+else:
+   print("Could not find CUDA_PATH in environment variables. Defaulting to /usr/local/cuda!")
+   CUDA_PATH = "/usr/local/cuda"
+
+if not os.path.isdir(CUDA_PATH):
+   print("CUDA_PATH {} not found. Please update the CUDA_PATH variable and rerun".format(CUDA_PATH))
+   exit(0)
+
+if not os.path.isdir(os.path.join(CUDA_PATH, "include")):
+    print("include directory not found in CUDA_PATH. Please update CUDA_PATH and try again")
+    exit(0)
+
+setup(name = 'rprof', version = '1.0',  \
+   ext_modules = [
+      Extension('rprof', ['rprof/rprof.c'], 
+      include_dirs=[np.get_include(), os.path.join(CUDA_PATH, "include")],
+      libraries=["rprof", "cudart"],
+      library_dirs = [".", os.path.join(CUDA_PATH, "lib64")]
+)])
