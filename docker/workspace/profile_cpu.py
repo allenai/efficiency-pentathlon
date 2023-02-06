90582a62d576fd0b74d5c6b4985e70ba19461499
Hao Peng <haop@allenai.org>
2023-02-06 14:34:13 -0800

    symplified cpu profiler

--- docker/workspace/profile_cpu.py
+++ docker/workspace/profile_cpu.py
@@ -3,18 +3,11 @@
 import pylikwid
 import time
 import sys
-import os
 import signal
-
-
-def sigterm_handler(_signo, _stack_frame):
-    sys.exit(0)
 
 
 if __name__ == "__main__":
     init_start = time.time()
-    signal.signal(signal.SIGTERM, sigterm_handler)
-    signal.signal(signal.SIGINT, sigterm_handler)
     pylikwid.inittopology()
     topodict = pylikwid.getcputopology()
 
@@ -27,37 +20,24 @@ if __name__ == "__main__":
     pylikwid.init(cpus)
     cpu_starts, cpu_stops = [], []
     dram_starts, dram_stops = [], []
-    for cpu in cpus:
-        cpu_starts.append(pylikwid.startpower(cpu, cpu_domainid))
-        dram_starts.append(pylikwid.startpower(cpu, dram_domainid))
-    start = time.time()
-    init_time = start - init_start
-    print(f"Init time: {init_time:.3f}s")
 
-    def _wrap_up():
-        time.sleep(init_time)
+    start = time.time()
+    def _signal_handler(sig, _frame):
         for cpu in cpus:
             cpu_stops.append(pylikwid.stoppower(cpu, cpu_domainid))
             dram_stops.append(pylikwid.stoppower(cpu, dram_domainid))
         time_elapsed = time.time() - start
-        with open(f"{os.getcwd()}/workspace/log/cpu.csv", "w") as fout:
-            print(f"Time elapsed: {time_elapsed:.3f}s\n")
-            fout.write("id,time_elapsed,cpu_energy,dram_energy\n")
-            for i in range(len(cpus)):
-                cpu_energy = pylikwid.getpower(cpu_starts[i], cpu_stops[i], cpu_domainid)
-                dram_energy = pylikwid.getpower(dram_starts[i], dram_stops[i], dram_domainid)
-                print(f"CPU {i}: {cpu_energy:.3f}\n")
-                print(f"DRAM {i}: {dram_energy:.3f}\n")
-                fout.write(f"{i},{time_elapsed:.3f},{cpu_energy:.3f},{dram_energy:.3f}\n")
-        sys.exit()
-    try:
-        while True:
-            time.sleep(0.1)
-    # except KeyboardInterrupt:
-    #     _wrap_up()
-    finally:
-        _wrap_up()
-        
+        results = {"time": time_elapsed}
+        for i in range(len(cpus)):
+            cpu_energy = pylikwid.getpower(cpu_starts[i], cpu_stops[i], cpu_domainid)
+            dram_energy = pylikwid.getpower(dram_starts[i], dram_stops[i], dram_domainid)
+            results[f"cpu_{i}"] = cpu_energy
+            results[f"dram_{i}"] = dram_energy
+        print(results)
+        sys.exit(0)
 
-    
-    
+    signal.signal(signal.SIGINT, _signal_handler)
+    for cpu in cpus:
+        cpu_starts.append(pylikwid.startpower(cpu, cpu_domainid))
+        dram_starts.append(pylikwid.startpower(cpu, dram_domainid))
+    signal.pause()
