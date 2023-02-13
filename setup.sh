407ecf475ea8464f1cc9e81d4188ebc2dec058ae
Hao Peng <haop@allenai.org>
2023-02-13 13:18:22 -0800

    update set up

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 conda create -n efficiency-benchmark python=3.9
+conda activate efficiency-benchmark
 
 conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
 
