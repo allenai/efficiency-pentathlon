a6a75f7fb494cac160484c8aeb78868c5d5eb9e4
Hao Peng <haop@allenai.org>
2023-02-13 13:17:12 -0800

    update set up

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+conda create -n efficiency-benchmark python=3.9
+
+conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
+
 cd docker
 docker build -t cpu_profiler .
 
