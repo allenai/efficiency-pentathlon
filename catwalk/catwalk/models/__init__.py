0580e0ea916a6fb0ba06ca95e8faaf86fa6c0b06
Hao Peng <haop@allenai.org>
2023-03-20 13:27:24 -0700

    fixed longformer huggingface naming

--- catwalk/catwalk/models/__init__.py
+++ catwalk/catwalk/models/__init__.py
@@ -1,6 +1,6 @@
 from typing import Dict
 from catwalk.model import Model
-from catwalk.models.bert_example import BertExample
+from catwalk.models.huggingface_classification import HuggingfaceClassification
 from catwalk.models.submission import Submission
 from catwalk.models.gpt import GPTModel
 from catwalk.models.t5 import T5
@@ -8,13 +8,23 @@ from catwalk.models.conditional_generation import ConditionalGenerationModel
 
 
 MODELS: Dict[str, Model] = {
-    "bert-example": BertExample(),
+    # "bert-example": BertExample(),
+    "models/mnli-bert-base": HuggingfaceClassification("models/mnli-bert-base"),
+    "models/mnli-bert-large": HuggingfaceClassification("models/mnli-bert-large"),
+    "models/mnli-deberta-small": HuggingfaceClassification("models/mnli-deberta-small"),
+    "models/mnli-deberta-base": HuggingfaceClassification("models/mnli-deberta-base"),
+    "models/mnli-deberta-large": HuggingfaceClassification("models/mnli-deberta-large"),
+    "models/mnli-roberta-base": HuggingfaceClassification("models/mnli-roberta-base"),
+    "models/mnli-roberta-large": HuggingfaceClassification("models/mnli-roberta-large"),
     "submission": Submission(),
     "gpt2": GPTModel("gpt2"),
     "conditional_generation": ConditionalGenerationModel("mbart"),
     "t5-small": T5("t5-small"),
+    "models/mnli-t5-small": T5("models/mnli-t5-small"),
     "t5-base": T5("t5-base"),
+    "models/mnli-t5-base": T5("models/mnli-t5-base"),
     "t5-large": T5("t5-large"),
+    "models/mnli-t5-large": T5("models/mnli-t5-large"),
     "t5-3b": T5("t5-3b"),
     "t5-11b": T5("t5-11b"),
     "flan-t5-small": T5("google/flan-t5-small"),
@@ -22,4 +32,7 @@ MODELS: Dict[str, Model] = {
     "flan-t5-large": T5("google/flan-t5-large"),
     "flan-t5-xl": T5("google/flan-t5-xl"),
     "flan-t5-xxl": T5("google/flan-t5-xxl"),
+    "longformer-base": HuggingfaceClassification("allenai/longformer-base-4096"),
+    "longformer-large": HuggingfaceClassification("allenai/longformer-large-4096"),
+    "distilbert-base": HuggingfaceClassification("distilbert-base-uncased")
 }
