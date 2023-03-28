d7e83ba87d0794535dd430322f48390332c7748b
Hao Peng <haop@allenai.org>
2023-03-28 12:07:31 -0700

    switched to codecarbon

--- catwalk/catwalk/steps.py
+++ catwalk/catwalk/steps.py
@@ -12,15 +12,12 @@ from typing import Any, Dict, Iterable, Optional, Sequence, Tuple, Union
 from tango.common.sequences import MappedSequence
 
 import torch
-from codecarbon import track_emissions
-
-import docker
-from catwalk.efficiency.carbon import get_realtime_carbon  # (TODO)
 from catwalk.model import Model
 from catwalk.models import MODELS
 from catwalk.task import Task
 from catwalk.tasks import TASKS
 from catwalk.tasks import InstanceFormat
+from codecarbon import EmissionsTracker
 
 
 EFFICIENCY_DIR = f"{pathlib.Path(__file__).parent.resolve()}/efficiency"  # TODO
@@ -47,6 +44,10 @@ class PredictStep():
         num_latency_instances = min(NUM_LATENCY_INSTANCES, len(self._eval_instances))
         self._latency_instances = Random(random_subsample_seed).sample(
                 self._eval_instances, num_latency_instances)
+        self._emission_tracker = EmissionsTracker(
+            measure_power_secs=0.1,
+            log_level="warning"
+        )
 
     def _get_instances(self) -> Tuple[Sequence[Dict[str, Any]], Sequence[str]]:
         instances = self.task.get_split(self.split)
@@ -81,48 +82,22 @@ class PredictStep():
     def start_profiling(
         self,
     ):
-        # try:
-        #     client = docker.from_env()
-        #     self._container = client.containers.run(
-        #         "cpu_profiler:latest",
-        #         "python3 profile_cpu.py",
-        #         name="cpu_profiler",
-        #         privileged=True,
-        #         tty=True,
-        #         remove=True,
-        #         detach=True,
-        #         stdout=True,
-        #         stderr=True
-        #     )
-        # except:
-        self._container = None
         self._p_gpu = subprocess.Popen(
             [f"{sys.executable}", f"{EFFICIENCY_DIR}/profile_gpu.py"],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             shell=False
         )
+        self._emission_tracker.start()
         self._start_time = time.time()
 
     def end_profiling(self, num_instances: int):
         time_elapsed = time.time() - self._start_time
+        self._emission_tracker.stop()
+
         os.kill(self._p_gpu.pid, signal.SIGTERM)
         if not self._p_gpu.poll():
             print("GPU monitor correctly halted")
-
-        if self._container is not None:
-            self._container.kill(signal.SIGINT)
-            cpu_results = self._container.logs().strip().decode("UTF-8")
-            if cpu_results == "":
-                cpu_energy = dram_energy = -999.0
-            else:
-                cpu_results = json.loads(cpu_results.replace("\'", "\""))
-                cpu_energy = cpu_results["cpu_energy"] / 3600.0  # Wh
-                dram_energy = cpu_results["dram_energy"] / 3600.0  # Wh
-            self._container.stop()
-        else:
-            cpu_energy = -999.0
-            dram_energy = -999.0
 
         self._p_gpu.wait()
         gpu_results = (self._p_gpu
@@ -132,7 +107,6 @@ class PredictStep():
                        .replace("\'", "\""))
         reader = csv.DictReader(gpu_results.split("\n"), delimiter=",")
         gpu_results = [r for r in reader]
-
         gpu_energy, max_gpu_mem = 0, 0
         try:
             for g in gpu_results:
@@ -142,16 +116,16 @@ class PredictStep():
         except KeyError:
             gpu_energy, max_gpu_mem = -999.0, -999.0
 
-        total_energy = gpu_energy  #  + cpu_energy + dram_energy
-        carbon = get_realtime_carbon(total_energy)  # in g
+        emission_data = self._emission_tracker.final_emissions_data
         return {
             "time": time_elapsed,
             "max_gpu_mem": max_gpu_mem,
-            "gpu_energy": gpu_energy,  # Wh
-            "cpu_energy": cpu_energy,  # Wh
-            "dram_energy": dram_energy,  # Wh
-            "total_energy": total_energy,
-            "carbon": carbon,
+            "gpu_energy": emission_data.gpu_energy,  # kWh
+            "rprof_gpu_energy": gpu_energy,
+            "cpu_energy": emission_data.cpu_energy,  # kWh
+            "dram_energy": emission_data.ram_energy,  # kWh
+            "total_energy": emission_data.gpu_energy + emission_data.cpu_energy + emission_data.ram_energy,
+            "carbon": emission_data.emissions,  # kg
             "throughput": num_instances / time_elapsed
         }
 
@@ -171,6 +145,8 @@ class PredictStep():
         print(f"Throughput: {efficiency_metrics['throughput']: .2f} instances / s.")
         print(f"Latency: {efficiency_metrics['latency'] * 1000: .2f} ms.")
 
+        print(f"{efficiency_metrics['rprof_gpu_energy']: .2f}, {efficiency_metrics['gpu_energy'] * 1000.: .2f}")
+
     def run(
         self,
         **kwargs
@@ -178,7 +154,7 @@ class PredictStep():
         output_batches = []
         self.start_profiling()
         try:
-            for output_batch in self.model.predict(instances=self._eval_instances, **kwargs):
+            for output_batch in self.model.predict(instances=self._latency_instances, **kwargs):
                 output_batches.append(output_batch)
             efficiency_metrics = self.end_profiling(num_instances=len(self._eval_instances))
         except:
