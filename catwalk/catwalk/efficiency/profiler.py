e14f9c388b6d5d86a2b791b05088f48dda3382e3
Hao Peng <haop@allenai.org>
2023-03-30 11:38:39 -0700

    rewored profiler based on codecarbon

---
+++ catwalk/catwalk/efficiency/profiler.py
@@ -0,0 +1,89 @@
+import time
+from typing import Any, Dict, Iterable, List, Optional
+from codecarbon import EmissionsTracker
+from codecarbon.core.gpu import get_gpu_details
+from codecarbon.core.gpu import is_gpu_details_available
+from codecarbon.external.scheduler import PeriodicScheduler
+
+
+NUM_LATENCY_INSTANCES = 32
+
+
+"""A wrapper of codecarbon EmissionsTracker aiming to provide GPU memory and untilization data."""
+class Profiler():
+
+    def __init__(
+            self,
+            interval: float = 0.1,
+            # gpu_ids: Optional[Iterable[int]] = None,
+            **kwargs):
+        # self.gpu_ids = gpu_ids
+        self._start_time: Optional[float] = None
+        self._emission_tracker = EmissionsTracker(
+            measure_power_secs=interval,
+            log_level="warning",
+            # gpu_ids=gpu_ids
+            **kwargs
+        )
+        self._gpu_details_available: bool = is_gpu_details_available()
+        self._gpu_scheduler: Optional[PeriodicScheduler] = None
+        self._max_used_gpu_memory: Optional[float] = None
+        self._gpu_utilization: Optional[float] = None
+        self._gpu_utilization_count: Optional[int] = None
+
+        if self._gpu_details_available:
+            self._gpu_scheduler = PeriodicScheduler(
+                function=self._profile_gpu,
+                interval=interval,
+            )
+            self._max_used_gpu_memory = -1.0
+            self._gpu_utilization = 0.0
+            self._gpu_utilization_count = 0
+
+    def _profile_gpu(self):
+        all_gpu_details: List[Dict] = get_gpu_details()
+        used_memory = sum(
+            [
+                gpu_details["used_memory"]
+                for idx, gpu_details in enumerate(all_gpu_details)
+                # if idx in self.gpu_ids
+             ]
+        )
+        gpu_utilization = sum(
+            [
+                gpu_details["gpu_utilization"]
+                for idx, gpu_details in enumerate(all_gpu_details)
+                # if idx in self.gpu_ids
+             ]
+        )
+        self._max_used_gpu_memory = max(self._max_used_gpu_memory, used_memory)
+        self._gpu_utilization += gpu_utilization
+        self._gpu_utilization_count += 1
+
+    def start(self) -> None:
+        self._emission_tracker.start()
+        if self._gpu_details_available:
+            self._gpu_scheduler.start()
+        self._start_time = time.time()
+
+    def stop(self) -> Dict[str, Any]:
+        time_elapsed = time.time() - self._start_time
+        self._emission_tracker.stop()
+        try:
+            self._gpu_scheduler.stop()
+        except:
+            raise RuntimeError("Failed to stop gpu scheduler.")
+        self._profile_gpu()
+        self._max_used_gpu_memory / self._max_used_gpu_memory / 2 ** 30
+        self._gpu_utilization /= self._gpu_utilization_count
+        codecarbon_data = self._emission_tracker.final_emissions_data
+        self.efficiency_metrics: Dict[str, Any] = {
+            "time": time_elapsed,
+            "max_gpu_mem": self._max_used_gpu_memory,
+            "gpu_energy": codecarbon_data.gpu_energy,  # kWh
+            "cpu_energy": codecarbon_data.cpu_energy,  # kWh
+            "dram_energy": codecarbon_data.ram_energy,  # kWh
+            "total_energy": codecarbon_data.gpu_energy + codecarbon_data.cpu_energy + codecarbon_data.ram_energy,
+            "carbon": codecarbon_data.emissions
+        }
+        return self.efficiency_metrics
--- catwalk/catwalk/steps.py
+++ catwalk/catwalk/steps.py
@@ -1,10 +1,4 @@
-import csv
 import json
-import os
-import pathlib
-import signal
-import subprocess
-import sys
 import time
 from collections import defaultdict
 from random import Random
@@ -17,11 +11,8 @@ from catwalk.models import MODELS
 from catwalk.task import Task
 from catwalk.tasks import TASKS
 from catwalk.tasks import InstanceFormat
-from codecarbon import EmissionsTracker
-
-
-EFFICIENCY_DIR = f"{pathlib.Path(__file__).parent.resolve()}/efficiency"  # TODO
-NUM_LATENCY_INSTANCES = 100
+from catwalk.efficiency.profiler import Profiler
+from catwalk.efficiency.profiler import NUM_LATENCY_INSTANCES
 
 
 class PredictStep():
@@ -44,9 +35,8 @@ class PredictStep():
         num_latency_instances = min(NUM_LATENCY_INSTANCES, len(self._eval_instances))
         self._latency_instances = Random(random_subsample_seed).sample(
                 self._eval_instances, num_latency_instances)
-        self._emission_tracker = EmissionsTracker(
-            measure_power_secs=0.1,
-            log_level="warning"
+        self._profiler = Profiler(
+            interval=0.1,
         )
 
     def _get_instances(self) -> Tuple[Sequence[Dict[str, Any]], Sequence[str]]:
@@ -79,56 +69,6 @@ class PredictStep():
             kwargs["split"] = kwargs["task"].default_split
         return kwargs
 
-    def start_profiling(
-        self,
-    ):
-        self._p_gpu = subprocess.Popen(
-            [f"{sys.executable}", f"{EFFICIENCY_DIR}/profile_gpu.py"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            shell=False
-        )
-        self._emission_tracker.start()
-        self._start_time = time.time()
-
-    def end_profiling(self, num_instances: int):
-        time_elapsed = time.time() - self._start_time
-        self._emission_tracker.stop()
-
-        os.kill(self._p_gpu.pid, signal.SIGTERM)
-        if not self._p_gpu.poll():
-            print("GPU monitor correctly halted")
-
-        self._p_gpu.wait()
-        gpu_results = (self._p_gpu
-                       .communicate()[0]
-                       .strip()
-                       .decode('UTF-8')
-                       .replace("\'", "\""))
-        reader = csv.DictReader(gpu_results.split("\n"), delimiter=",")
-        gpu_results = [r for r in reader]
-        gpu_energy, max_gpu_mem = 0, 0
-        try:
-            for g in gpu_results:
-                gpu_energy += float(g["energy"])
-                max_gpu_mem += float(g["max_mem"])
-            gpu_energy = gpu_energy / 3600.0
-        except KeyError:
-            gpu_energy, max_gpu_mem = -999.0, -999.0
-
-        emission_data = self._emission_tracker.final_emissions_data
-        return {
-            "time": time_elapsed,
-            "max_gpu_mem": max_gpu_mem,
-            "gpu_energy": emission_data.gpu_energy,  # kWh
-            "rprof_gpu_energy": gpu_energy,
-            "cpu_energy": emission_data.cpu_energy,  # kWh
-            "dram_energy": emission_data.ram_energy,  # kWh
-            "total_energy": emission_data.gpu_energy + emission_data.cpu_energy + emission_data.ram_energy,
-            "carbon": emission_data.emissions,  # kg
-            "throughput": num_instances / time_elapsed
-        }
-
     def tabulate_efficiency_metrics(
         self,
         efficiency_metrics: Dict[str, Any]
@@ -145,24 +85,22 @@ class PredictStep():
         print(f"Throughput: {efficiency_metrics['throughput']: .2f} instances / s.")
         print(f"Latency: {efficiency_metrics['latency'] * 1000: .2f} ms.")
 
-        print(f"{efficiency_metrics['rprof_gpu_energy']: .2f}, {efficiency_metrics['gpu_energy'] * 1000.: .2f}")
-
     def run(
         self,
         **kwargs
     ) -> Sequence[Any]:
         output_batches = []
-        self.start_profiling()
-        try:
-            for output_batch in self.model.predict(instances=self._latency_instances, **kwargs):
-                output_batches.append(output_batch)
-            efficiency_metrics = self.end_profiling(num_instances=len(self._eval_instances))
-        except:
-            self.end_profiling(num_instances=len(self._eval_instances))
+        self._profiler.start()
+        for output_batch in self.model.predict(instances=self._eval_instances, **kwargs):
+            output_batches.append(output_batch)
+        efficiency_metrics = self._profiler.stop()
+        efficiency_metrics["throughput"] = len(self._latency_instances) / efficiency_metrics["time"]
+
         ### Latency ###
         start_time = time.time()
         for output_batch in self.model.predict(instances=self._latency_instances, batch_size=1):
             pass
+        self.model.stop()
         elapsed_time = time.time() - start_time
         efficiency_metrics["latency"] = elapsed_time / len(self._latency_instances)
         # TODO
